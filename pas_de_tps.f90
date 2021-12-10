bd9d3eeb71bc14a264c2a3d1f04f3a8e9feaae2f
Basile-Desmolin <79247388+Basile-Desmolin@users.noreply.github.com>
2021-12-10 16:36:11 +0100

    Add files via upload
    fonction pas de temps, calculée avec le CFL

---
+++ pas_de_tps.f90
@@ -0,0 +1,11 @@
+function pas_de_tps(u,v,nu,dx,dy,nx,ny)
+	implicit none
+	
+	real*8 :: dt
+	real*8, dimension(nx+1,ny+1), intent(in) :: u,v
+	real*8 , intent(in) :: nu, dx, dy
+	integer*8 , intent(in):: nx, ny
+	
+	dt = 1/(2*(((max(abs(u))/dx) + (max(abs(v))/dy) + 2*nu*((1/(dx*dx)) + (1/(dy*dy))))
+
+end function pas_de_tps
