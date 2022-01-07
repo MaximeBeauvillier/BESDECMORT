6e84ed6289be033c2fb0748ad244edacd46e1d9a
Doriantra <79248153+Doriantra@users.noreply.github.com>
2022-01-07 16:12:54 +0100

    Add files via upload

--- profile.f90
+++ profile.f90
@@ -9,9 +9,8 @@ subroutine profiles(u,v,xx,yy,nx,ny,Re)
 
 	
 	
-	open(10,file='uprofile_Re='//CHAR(int(Re))//'_nx='//CHAR(nx)//'.dat')
+	open(10,file='uprofile.dat')
 	imilieu=ceiling(float(nx)/2.)
-	write(10,*)'y','u'
 	
 	do j=1,ny
 		write(10,*)yy(j),u(imilieu,j)
@@ -20,7 +19,6 @@ subroutine profiles(u,v,xx,yy,nx,ny,Re)
 	
 	open(20,file='vprofile.dat')
 	jmilieu=ceiling(float(ny)/2.)
-	write(20,*)'x','v'
 	
 	do i=1,nx
 		write(20,*)xx(i),v(i,jmilieu)
