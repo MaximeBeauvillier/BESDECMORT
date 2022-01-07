1f23f2d8637b27a7be97a240912b7f00a49eb964
Basile-Desmolin <79247388+Basile-Desmolin@users.noreply.github.com>
2022-01-07 17:56:57 +0100

    Add files via upload
    Nouveau script

--- script_python.py
+++ script_python.py
@@ -3,34 +3,41 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 
-Nx = [16]
-Ny = [16]
+Nx = [16,32]
+Ny = [16,32]
 
 def recup_valeur(data,Nx,norme):
     
-	data = open(data,"r") 
-	contenu_data = data.read()
-	data.close()
-	liste_data = contenu_data.split("\n")
-	print(liste)
+	valeur_data = np.loadtxt(data)
+	temps = np.loadtxt(path+'temps.dat')
+	it_max = min(len(temps),len(valeur_data))
+	plt.scatter(temps[1:it_max], valeur_data[1:it_max], marker = 'x',s=2)
+	plt.title("Energie cinétique en fonction du temps")
+	plt.xlabel("temps [s]")
+	plt.ylabel("Energie cinétique")
+	plt.savefig("Energie_cinetique_avec_Nx_"+str(Nx)+".png" )
+
 	
 
 
-path = "/home/basile/Documents/Ecole_ing/3A/incompressible/v2/BES_MNEI-main"
+path = "/home/bdesmoli/nosave/incompressible/BES_MNEI-main/"
 
 for i in range(len(Nx)):
-	f=open (path + "param.txt",'w')	
-	f.write(str(Nx[i])+'\n')
-	f.write(str(Ny[i]) + '\n') 
-	f.write(str(1) + '\n') #Nz
-	f.write(str(1) + '\n')  #schema (1-> upwind, 2 ->centre)
-	f.write(str(400.) + '\n')  #Re
-	f.write(str(10.)) #tf
-	f.writer(str(1e-4)) #epsilon
-	f.close()
-	os.system("./cavite.exe")
-	recup_resid_valeur(path + "kinetic.dat", Nx[i])
-	os.system("rm *.scl")
-	os.system("rm *.vec")
-	os.system("rm *.geo")
-	os.system("rm *.case")
+    f=open (path + "param.txt",'w')	
+    f.write(str(Nx[i])+'\n')
+    f.write(str(Ny[i]) + '\n') 
+    f.write(str(1) + '\n') #Nz
+    f.write(str(1) + '\n')  #schema (1-> upwind, 2 ->centre)
+    f.write(str(400.) + '\n')  #Re
+    f.write(str(15.) + '\n') #tf
+    f.write(str(0.1)+ '\n') #storage
+    f.write(str(0.001)) #epsilon
+    f.close()
+    os.system("make")
+    os.system("./cavite.exe")
+    recup_valeur(path + "kinetic.dat", Nx[i], "kinetic")
+    os.system("mkdir maillage_"+str(Nx[i]))
+    os.system("mv *.dat maillage_"+str(Nx[i]))
+    os.system("mv *.png maillage_"+str(Nx[i]))
+    os.system("make clean")
+
